ef337a2026d07688f9b1e598d091c9a95964e528
TogiFerretFerret <114247361+TogiFerretFerret@users.noreply.github.com>
2023-12-06 11:24:05 -0500

    Update fbdt.js

--- auth/fbdt.js
+++ auth/fbdt.js
@@ -1,3 +1,9 @@
 const firebaseConfig = {
-  apiKey: ""
-}
+    apiKey: "AIzaSyBnaL4-llOX3r1S7UdNGmYEnX1mPjjkdhM",
+    authDomain: "huntermsscibowl.firebaseapp.com",
+    projectId: "huntermsscibowl",
+    storageBucket: "huntermsscibowl.appspot.com",
+    messagingSenderId: "699196230257",
+    appId: "1:699196230257:web:8d09f536ec6bfd249169d0",
+    measurementId: "G-N1KFDPGXNQ"
+  };
